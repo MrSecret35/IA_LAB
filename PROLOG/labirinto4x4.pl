0f003a0b4745d7c82100b7157920743e88809fec
MrSecret35 <meccagiorgio@gmail.com>
2024-05-22 12:52:55 +0200

    AStar

---
+++ PROLOG/labirinto4x4.pl
@@ -0,0 +1,7 @@
+num_righe(4).
+num_colonne(4).
+
+iniziale(pos(4,2)).
+finale(pos(1,2)).
+
+occupata(pos(4,1)).
--- PROLOG/ricercaAStar.pl
+++ PROLOG/ricercaAStar.pl
@@ -5,7 +5,8 @@ ricerca(Cammino):-
     ricercaAStar([(S0,0,H, [])], [], Cammino).
 
 ricercaAStar([(S,_,_, Cammino)| _], _, Cammino):-
-    finale(S),!.
+    finale(S).
+
 /*
 ric_prof_lim([(S,G,H, Cammino)| Open], Closed, Risultato):-
     azione applicabile
@@ -20,28 +21,86 @@ ric_prof_lim([(S,G,H, Cammino)| Open], Closed, Risultato):-
     andiamo al prossimo stato minore 
 */
 
+/*
 ricercaAStar([(S,G,H, Cammino)| Open], Closed, Risultato):-
     applicabile(Az,S),
     trasforma(Az,S,SNuovo),
-    euristica(SNuovo,Euristica),
-    %CostoSNuovo is G+1+Euristica,
+    euristica(SNuovo,HNuovo),
+    %CostoSNuovo is G+1+HNuovo,
     \+ member(SNuovo,Closed),
-    %aggiungi((Snuovo,G+1,Euristica,[Az|Cammino]), Open, ListaNuova),
+    %aggiungi((Snuovo,G+1,HNuovo,[Az|Cammino]), Open, ListaNuova),
     %ric_prof_lim(ListaNuova, [(S,G,H, Cammino)| Closed], Risultato).
-    ric_prof_lim([(SNuovo,G+1,Euristica,[Az|Cammino]) | Open], [(S,G,H, Cammino)| Closed], Risultato).
+    ricercaAStar([(SNuovo,G+1,HNuovo,[Az|Cammino]) | Open], [(S,G,H, Cammino)| Closed], Risultato).
 
 ricercaAStar([(S,G,H, Cammino)| Open], Closed, Risultato):-
     applicabile(Az,S),
     trasforma(Az,S,SNuovo),
-    euristica(SNuovo,Euristica),
-    CostoSNuovo is G+1+Euristica,
+    euristica(SNuovo,HNuovo),
+    CostoSNuovo is G+1+HNuovo,
     member(SNuovo,Closed),
     restituisci(SNuovo,Closed,(SNuovo,GSNuovo,HSNUovo,_)),
     CostoSNuovoS is GSNuovo + HSNUovo,
     CostoSNuovo<CostoSNuovoS,
-    %aggiungi((Snuovo,G+1,Euristica,[Az|Cammino]), Open, ListaNuova),
+    %aggiungi((Snuovo,G+1,HNuovo,[Az|Cammino]), Open, ListaNuova),
     %ric_prof_lim(ListaNuova, [(S,G,H, Cammino)| Closed], Risultato).
-    ric_prof_lim([(SNuovo,G+1,Euristica,[Az|Cammino]) | Open], [(S,G,H, Cammino)| Closed], Risultato).
+    ricercaAStar([(SNuovo,G+1,HNuovo,[Az|Cammino]) | Open], [(S,G,H, Cammino)| Closed], Risultato).
+*/
+
+
+ricercaAStar([(S,G,H, Cammino)| Open], Closed, Risultato):-
+    findall(Az, applicabile(Az,S),ElencoAz),
+    elabora((S,G,H, Cammino), ElencoAz, Open, Closed, ListaNuovaOpen),
+    ricercaAStar(ListaNuovaOpen, [(S,G,H, Cammino)| Closed], Risultato).
+
+
+elabora((S,G,H,C), [Az| ElencoAz], Open, Closed, Res):-
+    trasforma(Az,S,SNuovo),
+    euristica(SNuovo,HNuovo),
+    \+ member((SNuovo,_,_,_), Closed),
+    aggiungi((SNuovo,G+1,HNuovo,[Az|C]), Open, ListaNuovaOpen),
+    elabora((S,G,H,C), ElencoAz, ListaNuovaOpen, Closed, Res).
+
+elabora((S,G,H,C), [Az| ElencoAz], Open, Closed, Res):-
+    trasforma(Az,S,SNuovo),
+    euristica(SNuovo,HNuovo),
+    CostoSNuovo is G+1+HNuovo,
+    member((SNuovo,_,_, _), Closed),
+    restituisci(SNuovo,Closed,(SNuovo,G_,H_,_)),
+    CostoSNuovo_ is G_ + H_,
+    CostoSNuovo < CostoSNuovo_,
+    aggiungi((SNuovo,G+1,HNuovo,[Az|C]), Open, ListaNuovaOpen),
+    elabora((S,G,H,C), ElencoAz, ListaNuovaOpen, Closed, Res).
+
+elabora((S,G,H,C), [Az| ElencoAz], Open, Closed, Res):-
+    trasforma(Az,S,SNuovo),
+    euristica(SNuovo,HNuovo),
+    CostoSNuovo is G+1+HNuovo,
+    member((SNuovo,_,_, _), Closed),
+    restituisci(SNuovo,Closed,(SNuovo,G_,H_,_)),
+    CostoSNuovo_ is G_ + H_,
+    CostoSNuovo >= CostoSNuovo_,
+    elabora((S,G,H,C), ElencoAz, Open, Closed, Res).
+
+elabora(_, [], Open, _, Open).
+
+aggiungi((S,G,H,C), [(S_,G_,H_,C_)| Open], [(S,G,H,C)|[(S_,G_,H_,C_)|Open] ]):-
+    Costo is G+H,
+    Costo=<G_ + H_.
+
+aggiungi((S,G,H,C), [], [(S,G,H,C)]).
+
+aggiungi((S,G,H,C), [(S_,G_,H_,C_)| Open], [(S_,G_,H_,C_)| Lista]):-
+    Costo is G+H,
+    Costo>G_ + H_,
+    aggiungi((S,G,H,C), Open, Lista).
+
+%Dato uno stato e una lista Restituisci i valori corrispondenti a quello stato
+restituisci(S,[(S,G,H,C)| _],(S,G,H,C)).
+
+restituisci(S,[(SX,_,_,_)| Closed],(S,G,H,C)):-
+    S \== SX, 
+    restituisci(S, Closed, (S,G,H,C)).
+
 
 euristica(pos(X,Y),Ris):-
     finale(pos(X1,Y1)),
