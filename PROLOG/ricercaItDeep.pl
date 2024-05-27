87e863d9cca76f897c9fdcbf5623025aed6a03f5
MrSecret35 <meccagiorgio@gmail.com>
2024-05-27 14:19:06 +0200

    ItDeep OK

--- PROLOG/ricercaItDeep.pl
+++ PROLOG/ricercaItDeep.pl
@@ -1,18 +1,23 @@
-
 %wrapper
 ricerca(Cammino,Soglia):-
     iniziale(S0),
-    ric_prof_lim(S0,Soglia,Cammino,[]).
+    ric_prof_lim(S0,Soglia,Cammino,[],StatoFinale),
+    finale(StatoFinale).
+
+ricerca(Cammino,Soglia):-
+    iniziale(S0),
+    ric_prof_lim(S0,Soglia,_,[],StatoFinale),
+    \+ finale(StatoFinale),
+    SogliaN is Soglia+1,
+    ricerca(Cammino,SogliaN).
 
 %ricerca in profondità
-%caso 0: siamo nello stato finale
-ric_prof_lim(S,_,[],_):-
-    finale(S),!.
+ric_prof_lim(S,0,[],_,S).
 
-ric_prof_lim(S,Soglia,[Az|ListaAzioni],ListaStatiVisitati):-
+ric_prof_lim(S,Soglia,[Az|ListaAzioni],ListaStatiVisitati,StatoFinale):-
     Soglia>0,
     applicabile(Az,S),
     trasforma(Az,S,SNuovo),
     \+ member(SNuovo,ListaStatiVisitati),
     SogliaN is Soglia-1,
-    ric_prof_lim(SNuovo,SogliaN,ListaAzioni,[SNuovo|ListaStatiVisitati]).
+    ric_prof_lim(SNuovo,SogliaN,ListaAzioni,[SNuovo|ListaStatiVisitati],StatoFinale).
