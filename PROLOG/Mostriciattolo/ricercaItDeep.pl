c9adea994afce1fb0a0930f6bd52849226c42fe4
MrSecret35 <meccagiorgio@gmail.com>
2024-05-28 19:59:34 +0200

    itDeep Mostro

--- PROLOG/Mostriciattolo/ricercaItDeep.pl
+++ PROLOG/Mostriciattolo/ricercaItDeep.pl
@@ -3,25 +3,26 @@ ricerca(Cammino):-
     Soglia is 0,
     leggiGiaccio([],Ghiaccio),
     martello(Martello),
+    % write("Giaccio: "),write(Ghiaccio),write("\n"),
     start(S0,Cammino1,Soglia,Martello,Ghiaccio,GhiaccioFinale),
-    write(Cammino1),
-    write("\n"),
-    write("Ho preso il martello"),
+    % write("Giaccio finale: "),write(GhiaccioFinale),write("\n"),
+    write("Ho preso il martello"),write("\n"),
     finale(Portale),
     start(Martello,Cammino2,Soglia,Portale,GhiaccioFinale,_),
-    write("\n"),
-    write(Cammino2),
+    
+    write(Cammino1),write("\n"),
+    write(Cammino2),write("\n"),
     append(Cammino1,Cammino2,Cammino).
 
 start(S0,Cammino,Soglia,Ricerca,Ghiaccio,GhiaccioFinale):-
     ric_prof_lim(S0,Soglia,Cammino,[],StatoFinale,Ghiaccio,GhiaccioFinale),
     Ricerca == StatoFinale.
 
-start(S0,Cammino,Soglia,Ricerca,Ghiaccio,GhiaccioFinale):-
-    ric_prof_lim(S0,Soglia,_,[],StatoFinaleN,Ghiaccio,GhiaccioFinale),
+start(S0,Cammino,Soglia,Ricerca,Ghiaccio,GhiaccioFinaleN):-
+    ric_prof_lim(S0,Soglia,_,[],StatoFinaleN,Ghiaccio,_),
     Ricerca \== StatoFinaleN,
     SogliaN is Soglia+1,!,
-    start(S0,Cammino,SogliaN,Ricerca,Ghiaccio,GhiaccioFinale).
+    start(S0,Cammino,SogliaN,Ricerca,Ghiaccio,GhiaccioFinaleN).
 
 
 %--------------------------------------------------------
@@ -34,7 +35,8 @@ ric_prof_lim(S,Soglia,[Az|ListaAzioni],ListaStatiVisitati,StatoFinale,Ghiaccio,G
     Soglia>0,
     \+ finale(S),
     applicabileTutto(Az,[ S | Ghiaccio ]),
-    trasformaMostro(Az,S,SNuovo,Ghiaccio,GhiaccioN,Soglia),
+    trasformaMostro(Az,S,SNuovo,Ghiaccio),
+    trasformaGhiaccio(Az,Ghiaccio,GhiaccioN, Ghiaccio),
     \+ member(SNuovo,ListaStatiVisitati),
     SogliaN is Soglia-1,
     ric_prof_lim(SNuovo,SogliaN,ListaAzioni,[SNuovo|ListaStatiVisitati],StatoFinale,GhiaccioN,GhiaccioFinale).
@@ -45,7 +47,35 @@ ric_prof_lim(S,Soglia,[Az|ListaAzioni],ListaStatiVisitati,StatoFinale,Ghiaccio,G
 %-----------------------Dominio--------------------------
 %--------------------------------------------------------
 
+trasformaMostro(Az,S,SNuovo,Ghiaccio):-
+    nApplicabili(Az,S,Ghiaccio,N),
+    trasformaN(Az,S,N,SNuovo).
 
+trasformaGhiaccio(Az,[G | ListaG],[SNuovo| NewLista],Ghiaccio):-
+    nApplicabili(Az,G,Ghiaccio,N),
+    trasformaN(Az,G,N,SNuovo),
+    trasformaGhiaccio(Az, ListaG, NewLista ,Ghiaccio).
+
+trasformaGhiaccio(_,[],[],_).
+
+% nApplicabili
+% calcola il numero di movimenti verso una azione che è possibile fare
+nApplicabili(Az,S,Ghiaccio,1+N):-
+    applicabile(Az,S),
+    trasforma(Az,S,SNuovo),
+    \+ member(SNuovo,Ghiaccio),
+    nApplicabili(Az,SNuovo,Ghiaccio,N).
+
+nApplicabili(Az,S,Ghiaccio,0+N):-
+    applicabile(Az,S),
+    trasforma(Az,S,SNuovo),
+    member(SNuovo,Ghiaccio),
+    nApplicabili(Az,SNuovo,Ghiaccio,N).
+
+nApplicabili(Az,S,_,0):-
+    \+ applicabile(Az,S).
+
+/*
 trasformaMostro(Az,S,SNuovo,Ghiaccio,GhiaccioN,Soglia):-
     Soglia>0,
     trasformaIT(Az,S,S1),
@@ -68,7 +98,7 @@ trasformaIT(Az,G,GNuovo):-
 
 trasformaIT(Az,G,G):-
     \+ applicabile(Az,G).
-
+*/
 %--------------------------------------------------------
 %--------------------Applicabile-------------------------
 %--------------------------------------------------------
@@ -83,13 +113,25 @@ applicabileStato(Az,S,Ghiaccio):-
     applicabile(Az,S),
     trasforma(Az,S,SNuovo),
     member(SNuovo,Ghiaccio),
-    appicabileMostro(Az,SNuovo,Ghiaccio).
+    applicabileStato(Az,SNuovo,Ghiaccio).
 
 applicabileStato(Az,S,Ghiaccio):-
     applicabile(Az,S),
     trasforma(Az,S,SNuovo),
     \+ member(SNuovo,Ghiaccio).
 
+%--------------------------------------------------------
+%----------------------Trasforma-------------------------
+%--------------------------------------------------------
+
+trasformaN(est,pos(R,C),N,pos(R,CDestra)):-CDestra is C+N.
+trasformaN(ovest,pos(R,C),N,pos(R,CSin)):-CSin is C-N.
+trasformaN(nord,pos(R,C),N,pos(RSopra,C)):-RSopra is R-N.
+trasformaN(sud,pos(R,C),N,pos(RSotto,C)):-RSotto is R+N.
+
+%--------------------------------------------------------
+%-------------------Leggi Ghiaccio-----------------------
+%--------------------------------------------------------
 
 leggiGiaccio(T,[G | Lista]):-
     ghiaccio(G),
