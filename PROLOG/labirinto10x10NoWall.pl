28d9292e922b19a286d4dc8b98b5607f698ad632
robertobombieri <roberto.bombieri@edu.unito.it>
2024-05-28 14:50:12 +0200

    Aggiunti alcuni labirinti

---
+++ PROLOG/labirinto10x10NoWall.pl
@@ -0,0 +1,7 @@
+num_righe(10).
+num_colonne(10).
+
+iniziale(pos(4,2)).
+finale(pos(7,9)).
+
+occupata(pos()).
--- PROLOG/labirinto4x4.pl
+++ PROLOG/labirinto4x4.pl
@@ -2,6 +2,7 @@ num_righe(4).
 num_colonne(4).
 
 iniziale(pos(4,2)).
+finale(pos(2,4)).
 finale(pos(1,4)).
 
 occupata(pos(1,1)).
