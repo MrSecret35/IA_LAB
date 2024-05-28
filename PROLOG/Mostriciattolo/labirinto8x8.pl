5bf31151ebc2538b9ee6e14ade2a0991f773ccc8
MrSecret35 <meccagiorgio@gmail.com>
2024-05-28 15:17:19 +0200

    labirinto mostro

---
+++ PROLOG/Mostriciattolo/labirinto8x8.pl
@@ -0,0 +1,9 @@
+ghiaccio(pos(2,6)).
+ghiaccio(pos(2,7)).
+ghiaccio(pos(7,7)).
+
+martello(pos(8,2)).
+
+gemma(pos(1,7)).
+gemma(pos(5,4)).
+gemma(pos(8,8)).
---
+++ PROLOG/run_mostro.sh
@@ -0,0 +1,2 @@
+#swipl './Labirinto/labirinto8x8.pl' './Labirinto/problemaLabirinto.pl' './Mostriciattolo/labirinto8x8.pl' './Mostriciattolo/ricercaAStar.pl'
+swipl './Labirinto/labirinto8x8.pl' './Labirinto/problemaLabirinto.pl' './Mostriciattolo/labirinto8x8.pl' './Mostriciattolo/ricercaItDeep.pl'
